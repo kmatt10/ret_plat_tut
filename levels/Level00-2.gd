4d98f0d33eb015aa8ebfabd87bee4e3f594d33c5
kmatt10 <kmatt10@gmail.com>
2023-02-22 18:33:52 -0600

    level 00-1
    Fixed level 00-1 properties

--- levels/Level00-2.gd
+++ levels/Level00-2.gd
@@ -3,4 +3,4 @@ extends Node2D
 onready var tip_text = $CanvasLayer/HUD/MarginContainer/HBoxContainer/LevelTips
 
 func _ready():
-	tip_text.text = """Catch the ball by making contact with it in flight"""
+	tip_text.text = """Try it all out!"""
